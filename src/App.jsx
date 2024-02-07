6bc1abff15cecb1017ccb4a80a04c0e133161d83
sayanrick <sayanrick100@gmail.com>
2024-02-07 14:41:20 +0530

    complete frontend

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Container from "react-bootstrap/Container";
+import NavBar from "./components/NavBar";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import VerifyEmail from "./pages/VerifyEmail";
+
+function App() {
+  const { user } = useContext(AuthContext);
+  return (
+    <>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={user ? <Chat /> : <Login />} />
+          <Route path="/register" element={user ? <Chat /> : <Register />} />
+          <Route path="/login" element={user ? <Chat /> : <Login />} />
+          <Route path="/verify-email" element={<VerifyEmail/>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
